fd0295146338920ff7a5843e280fdff1cf53f972
Moonzenith <alejandroq1113@gmail.com>
2019-05-27 13:58:53 -0500

    tipo de pesomax en DECLARE ya no esta quemado

--- script.sql
+++ script.sql
@@ -1,11 +1,15 @@
+--1.a
+
 CREATE OR REPLACE TRIGGER control_peso_producto
 BEFORE INSERT ON PRODUCTO
 FOR EACH ROW
 DECLARE
-    pesomax NUMBER;
+    pesomax CAMION.PESOMAX%TYPE;
 BEGIN
     SELECT MAX(pesomax) INTO pesomax FROM CAMION;
     IF(:NEW.peso > pesomax) THEN
         RAISE_APPLICATION_ERROR(-1313,'El peso de un producto no puede ser mayor al camion mas grande');
     END IF;
 END;
+
+--1.b
