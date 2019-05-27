8af0b7be47cbd48b89b067288041b84b8016416e
Moonzenith <alejandroq1113@gmail.com>
2019-05-27 12:55:55 -0500

    punto 1 a

---
+++ script.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TRIGGER control_peso_producto
+BEFORE INSERT ON PRODUCTO
+FOR EACH ROW
+DECLARE
+    pesomax NUMBER;
+BEGIN
+    SELECT MAX(pesomax) INTO pesomax FROM CAMION;
+    IF(:NEW.peso > pesomax) THEN
+        RAISE_APPLICATION_ERROR(-1313,'El peso de un producto no puede ser mayor al camion mas grande');
+    ELSE
+        INSERT INTO PRODUCTO :NEW;
+    END IF;
+END;
