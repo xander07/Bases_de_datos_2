5eb04bb6e514fa9909cf9c4f41ac7079eaca5229
Moonzenith <alejandroq1113@gmail.com>
2019-05-27 14:28:36 -0500

    Trigger update peso

--- script.sql
+++ script.sql
@@ -12,4 +12,17 @@ BEGIN
     END IF;
 END;
 
+CREATE OR REPLACE TRIGGER control_update_peso_producto
+BEFORE UPDATE OF peso ON PRODUCTO
+FOR EACH ROW
+DECLARE
+    pesomax CAMION.PESOMAX%TYPE;
+BEGIN
+    SELECT MAX(pesomax) INTO pesomax FROM CAMION;
+    IF(:NEW.peso > pesomax) THEN
+        RAISE_APPLICATION_ERROR(-1313,'El peso de un producto no puede ser mayor al camion mas grande');
+    END IF;
+END;
+
+
 --1.b
